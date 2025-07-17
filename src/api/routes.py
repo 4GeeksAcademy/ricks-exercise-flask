f2443621e06d20b76efb860218977579f8c836b0
Ricky <rikigibson199@gmail.com>
2025-07-17 15:31:42 +0000

    finalizado

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,14 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity
+)
+
+
+#pip install flask-jwt-extended
 
 api = Blueprint('api', __name__)
 
--- src/app.py
+++ src/app.py
@@ -10,16 +10,26 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import (
+    JWTManager,
+)
 
-# from models import Person
-
+from datetime import timedelta
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-# database condiguration
+
+app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'tu_super_clave_secreta')  # Cambia esto!
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=1)  # Tokens expiran en 1 hora
+
+
+jwt = JWTManager(app)
+
+
+
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
